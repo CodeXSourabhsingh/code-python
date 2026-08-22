a0ecab6d96ab14d2b69a63ffa5cb7bba2cf88cac
Sourabh Singh <singhsourabh30542@gmail.com>
2026-08-22 15:42:51 +0530

    Add files via upload
    day7

---
+++ calculator (2).py
@@ -0,0 +1,12 @@
+a =54
+b = 56
+c = 36
+d = 96
+print("the addition of 'a+b'is",a+b)
+print("subtraction of a-b is",a-b)
+print("mltiplication of a*b is",a*b)
+print("division of a/b is",a/b)
+print(a+c)
+print(a-d)
+print(a*c)
+print(a/b)
